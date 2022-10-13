657f032734c8cb8ead2ae854e13e007cfd08737f
sallanez <saul.alejandro19@gmail.com>
2022-10-13 12:51:31 -0600

    Files added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.css';
+import './images/headphone.png'
+import './images/weight.png'
+import './images/heart.png'
 
 console.log('Hello world');
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname,'dist'),
